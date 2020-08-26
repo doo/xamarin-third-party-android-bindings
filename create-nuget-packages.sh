e6754ca3337d1f5a5ceab21b5ed0ccca2e405080
Nikituh <aare.undo@gmail.com>
2020-08-26 18:42:16 +0200

    sdk.dependencies: rm moshi, added kotlin, dynamicanimation

--- create-nuget-packages.sh
+++ create-nuget-packages.sh
@@ -14,12 +14,12 @@ msbuild xamarin-third-party-android-bindings.sln /m:4 /p:Configuration=Release
 NUGET_DIST_TARGET_DIR="./nuget-dist"
 rm -rf $NUGET_DIST_TARGET_DIR
 mkdir -p $NUGET_DIST_TARGET_DIR
-nuget pack Scanbot.Xamarin.Kotlin.StdLib/Scanbot.Xamarin.Kotlin.StdLib.nuspec -OutputDirectory $NUGET_DIST_TARGET_DIR
-nuget pack Scanbot.Xamarin.Kotlin.StdLib.Jre7/Scanbot.Xamarin.Kotlin.StdLib.Jre7.nuspec -OutputDirectory $NUGET_DIST_TARGET_DIR
-nuget pack Scanbot.Xamarin.Kotlin.StdLib.Jre8/Scanbot.Xamarin.Kotlin.StdLib.Jre8.nuspec -OutputDirectory $NUGET_DIST_TARGET_DIR
-nuget pack Scanbot.Xamarin.Kotlin.StdLib.Jdk7/Scanbot.Xamarin.Kotlin.StdLib.Jdk7.nuspec -OutputDirectory $NUGET_DIST_TARGET_DIR
-nuget pack Scanbot.Xamarin.Kotlin.StdLib.Jdk8/Scanbot.Xamarin.Kotlin.StdLib.Jdk8.nuspec -OutputDirectory $NUGET_DIST_TARGET_DIR
-nuget pack Scanbot.Xamarin.JetBrains.Java.Annotations/Scanbot.Xamarin.JetBrains.Java.Annotations.nuspec -OutputDirectory $NUGET_DIST_TARGET_DIR
+# nuget pack Scanbot.Xamarin.Kotlin.StdLib/Scanbot.Xamarin.Kotlin.StdLib.nuspec -OutputDirectory $NUGET_DIST_TARGET_DIR
+# nuget pack Scanbot.Xamarin.Kotlin.StdLib.Jre7/Scanbot.Xamarin.Kotlin.StdLib.Jre7.nuspec -OutputDirectory $NUGET_DIST_TARGET_DIR
+# nuget pack Scanbot.Xamarin.Kotlin.StdLib.Jre8/Scanbot.Xamarin.Kotlin.StdLib.Jre8.nuspec -OutputDirectory $NUGET_DIST_TARGET_DIR
+# nuget pack Scanbot.Xamarin.Kotlin.StdLib.Jdk7/Scanbot.Xamarin.Kotlin.StdLib.Jdk7.nuspec -OutputDirectory $NUGET_DIST_TARGET_DIR
+# nuget pack Scanbot.Xamarin.Kotlin.StdLib.Jdk8/Scanbot.Xamarin.Kotlin.StdLib.Jdk8.nuspec -OutputDirectory $NUGET_DIST_TARGET_DIR
+# nuget pack Scanbot.Xamarin.JetBrains.Java.Annotations/Scanbot.Xamarin.JetBrains.Java.Annotations.nuspec -OutputDirectory $NUGET_DIST_TARGET_DIR
 nuget pack Scanbot.Xamarin.SDK.Dependencies/Scanbot.Xamarin.SDK.Dependencies.nuspec -OutputDirectory $NUGET_DIST_TARGET_DIR
 
 echo "NuGet results:"
