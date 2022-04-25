d337a7b7a69860d4ef4a53fe18dd0ba8df5892f1
Marco <marco@scanbot.io>
2022-04-25 16:10:00 +0200

    Added push-packages.sh script

---
+++ push-packages.sh
@@ -0,0 +1,27 @@
+API_KEY=$1
+
+if [ -z "$API_KEY" ]; then
+    echo "Please specify an api key"
+    exit 1;
+fi
+
+PACKAGES=$(find nuget-dist/Scanbot.Xamarin.*)
+
+echo
+echo "Found packages:"
+echo
+for i in $PACKAGES; do
+    echo "$i"
+done
+echo
+
+read -p "Are you sure you want to publish these packages(y/n)? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    for i in $PACKAGES; do
+        echo
+        echo "Uploading package $i"
+        echo
+        nuget push $i -ApiKey $API_KEY -Timeout 600 -Source nuget.org
+    done  
+fi
